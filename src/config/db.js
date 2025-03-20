37fdd5113344a083a4769dbf0bd6fd7d5146d120
luisfer74081@gmail.com <luisfer74081@gmail.com>
2025-03-19 23:17:58 -0500

    db ok

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,18 @@
-// config/db.js
 const mongoose = require('mongoose');
 require('dotenv').config(); // Cargar variables de entorno
 
-const { MONGO_URI, MONGO_USER, MONGO_PASS } = process.env;
-
 const connectDB = async () => {
   try {
+    // Verifica que MONGODB_URI esté definida
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI no está definida en el archivo .env');
+    }
+
     // Conectar a MongoDB usando la cadena de conexión del archivo .env
-    await mongoose.connect(process.env.MONGODB_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Conexión a MongoDB establecida');
   } catch (err) {
     console.error('Error conectando a MongoDB:', err.message);
